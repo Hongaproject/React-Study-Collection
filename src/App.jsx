823723aece55f0bf884d203beafbe705da8d0254
{Hongaproject} <{tkwlscjq99@gmail.com}>
2023-03-11 12:45:19 +0900

    2day props

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import './App.css';
 import HelloWorld from './HelloWorld';
+import Wrapper from './Wrapper';
 
 function App() {
-
-  const text = "1일차 입니다."
-  const style = {
-    backgroundColor: 'black',
-    color: '#fff',
-    padding: '20px'
-  }
   return (  
-    <div>
-      <HelloWorld/>
-      <div style={style}>{text}</div>
-      <div className='box'></div>
-    </div>
+    <Wrapper>
+      <HelloWorld name="2일차 입니다." color="aqua" backgroundColor="black" padding="1rem"/>
+    </Wrapper>
   );
 }
-
 export default App;
