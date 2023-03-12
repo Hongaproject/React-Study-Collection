d450e8ca16a6c3e3484f833bc6bb919a740c0c92
Hongaproject <tkwlscjq99@gmail.com>
2023-03-12 23:24:23 +0900

    3day counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Counter from './Counter';
 import HelloWorld from './HelloWorld';
 import Wrapper from './Wrapper';
 
 function App() {
   return (  
-    <Wrapper>
-      <HelloWorld name="2일차 입니다." color="aqua" backgroundColor="black" padding="1rem"/>
-    </Wrapper>
+    <div>
+      <Wrapper>
+        <HelloWorld name="2일차 입니다." color="aqua" backgroundColor="black" padding="1rem"/>
+      </Wrapper>
+      <Counter/>
+    </div>
   );
 }
 export default App;
