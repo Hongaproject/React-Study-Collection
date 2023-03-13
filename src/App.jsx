65c584fed411cd666a3c035579d475c01907e256
Hongaproject <tkwlscjq99@gmail.com>
2023-03-13 19:01:23 +0900

    4day input, userlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Counter from './Counter';
 import HelloWorld from './HelloWorld';
+import InputText from './InputText';
+import UserList1 from './UserList1';
 import Wrapper from './Wrapper';
 
 function App() {
@@ -8,7 +10,11 @@ function App() {
       <Wrapper>
         <HelloWorld name="2일차 입니다." color="aqua" backgroundColor="black" padding="1rem"/>
       </Wrapper>
-      <Counter/>
+      <Counter marginLeft="20px"/>
+      <br />
+      <InputText marginLeft ="20px"/>
+      <br />
+      <UserList1 marginLeft="20px"/>
     </div>
   );
 }
