6fb721b032f2cd4a4cd0deaeb9f61a8978e5949d
Hongaproject <tkwlscjq99@gmail.com>
2023-05-09 20:41:59 +0900

    movie

--- nomadcoder-React/elementary/src/components/Movie.jsx
+++ nomadcoder-React/elementary/src/components/Movie.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({year, title, summary, poster}){
+function Movie({year, title, summary, poster, genres}){
     return(
         // <div><h4>{title}</h4></div>
         
@@ -14,9 +14,14 @@ function Movie({year, title, summary, poster}){
 
         <div className="movie">
             <img src={poster} alt={title} title={title} />
-            <div className="movie_data">
+            <div className="movie_data">  
                 <h3 className="movie_title">{title}</h3>
                 <h5 className="movie_year">{year}</h5>
+                <ul className="genres">
+                    {genres.map(genre => (
+                        <li className="genres_genre">{genre}</li>
+                    ))}
+                </ul>
                 <p className="movie_summary">{summary}</p>
             </div>
         </div>
@@ -28,7 +33,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
