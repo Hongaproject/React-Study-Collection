8c70bc94d8d0ad7bb8f1135d69f4bbca2fc9d85d
Hongaproject <tkwlscjq99@gmail.com>
2023-07-11 23:15:25 +0900

    리액트props

--- blogtodo/src/App.jsx
+++ blogtodo/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <div>
       <h1 color='red'>리액트란?</h1>
       <h3 style={react} >페이스북에서 만든 자바스크립트 UI 라이브러리이다. 특징으로는 SPA CSR virtual DOM이 있다.</h3>
-      <Study />
+      <Study name="Component" color="aqua"/>
       <Test>
         <h1>리액트는 컴포넌트를 사용하여 유지보수가 좋으며 SPA를 이용하여 메모리 관리를 해주고 커뮤니티가 방대하다는 뜻은 안정성이 좋다는 것과 오류에 대한 정보를 얻을수있다.</h1>
       </Test>
--- blogtodo/src/Study.jsx
+++ blogtodo/src/Study.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Study() {
+function Study({name, color}) {
     return(
         <div>
-            <h1 >사용중입니다.</h1>
+            <h1 style={color}> {name}사용중입니다.</h1>
             <h3>레고조각처럼 조각조각 맞추어서 사용한다. 컴포넌트를 사용하면 유지보수가 용이하고 캡슐화, 재사용성, 결합성이 좋다.</h3>
         </div>
     );
