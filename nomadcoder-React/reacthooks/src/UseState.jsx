9858d28a6919e6359b9f5fc6750fd458d56da9b7
Hongaproject <tkwlscjq99@gmail.com>
2023-07-29 13:15:20 +0900

    배열

---
+++ nomadcoder-React/reacthooks/src/UseState.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+//useState는 항상 2개의 value를 return한다.
+function UseState(){
+
+    const [item, setItem] = useState(); //초기 값 세팅 할 수 있는 옵션 제공.
+
+    return(
+        <div></div>
+    );
+}
+
+export default UseState;
