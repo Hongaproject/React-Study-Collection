150a9af776a1dd42498a24bb350da2e1ee738406
Hongaproject <tkwlscjq99@gmail.com>
2023-07-31 11:14:29 +0900

    노마드코더 리액트 훅 useState 함수형, 클래스형 비교

--- nomadcoder-React/reacthooks/src/UseState.jsx
+++ nomadcoder-React/reacthooks/src/UseState.jsx
@@ -1,13 +1,58 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-//useState는 항상 2개의 value를 return한다.
-function UseState(){
+// const User = () => {
+//     return(
+//         <div></div>
+//     );
+// }
 
-    const [item, setItem] = useState(); //초기 값 세팅 할 수 있는 옵션 제공.
+//useState는 2개의 value값을 return한다.
+function UseState() {
 
+    const [item, setItem] = useState(1); //useState는 초기값 설정이 가능하다. 
+    const incrementItem = () => setItem(item + 1);
+    const decrementItem = () => setItem(item - 1);
+
+    //useState는 16.8버전 전에는 함수형 컴포넌트서 state를 사용할 수 없었음 
     return(
-        <div></div>
+        <div>
+            <h1>{item}</h1>
+            <button onClick={incrementItem}>+</button>
+            <button onClick={decrementItem}>-</button>
+        </div>
     );
 }
+//class와 function 컴포넌트 비교
+
+class UseStateClass extends React.Component{
+    state ={
+        item: 1
+    }
+    render(){
+        const {item} = this.state;
+        return(
+            <div>
+                <h1>{item}</h1>
+                <button onClick={this.incrementItem}>+</button>
+                <button onClick={this.decrementItem}>-</button>
+            </div>
+        );
+    }
+    incrementItem = () => {
+        this.setState(state => {
+            return{
+                item: state.item + 1
+            };
+        });
+    };
+    decrementItem = () => {
+        this.setState(state => {
+            return{
+                item: state.item - 1
+            };
+        });
+    };
+}
+
 
 export default UseState;
