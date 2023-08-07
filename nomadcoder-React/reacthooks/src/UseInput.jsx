54c30320b315d2346b8142805ef1f6ffa4a100a7
Hongaproject <tkwlscjq99@gmail.com>
2023-08-07 13:32:29 +0900

    UseInput 검증

--- nomadcoder-React/reacthooks/src/UseInput.jsx
+++ nomadcoder-React/reacthooks/src/UseInput.jsx
@@ -1,17 +1,34 @@
 import { useState } from "react";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
     const [value, setValue] = useState(initialValue);
+
     const onChange = (e) => {
-        console.log(e.target)
+        const {
+            target: {value}
+        } = e;
+
+        let willUpdate = true; // true라 항상 업데이트가 진행된다.
+
+        if(typeof validator === "function"){ 
+            // 매번 validator을 값이 변하고 타입이 function이면 willUpdate에 validator의 결과를 업로드
+            willUpdate = validator(value); //유효성 검사 maxLen이 validator이다.
+        }
+        if(willUpdate){
+            setValue(value);
+        }
     }
+    // 특정문자 사용 못하게 하기 위해 검증기능을 사용함.
+
     return {value, onChange}; // useInput의 기본값을 value와 return 해준다.
 }
 
 function UseInput () { 
-
-    const name = useInput("H's ") 
+    const maxLen = (value) => value.length <= 10; //최대 길이 검증하는 함수, true나 false를 return 해줌
+ 
+    const name = useInput("H's ", maxLen) // name을 사용하여 useInput에서 H's와 maxLen을 넣어줌
     // input부분에 value={name.value}, {...name}을 적으면 useInput 값이 나옴.
+
     return(
         <div> 
             <h1>hello! world~</h1>
