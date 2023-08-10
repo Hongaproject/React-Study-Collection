8d5e1f6a400e2f1d03e611a54fdb5b8c64e0c7c4
Hongaproject <tkwlscjq99@gmail.com>
2023-08-10 15:34:35 +0900

    useState 응용 버튼 클릭시 내용 보여주기

--- nomadcoder-React/reacthooks/src/UseTabs.jsx
+++ nomadcoder-React/reacthooks/src/UseTabs.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { useState } from "react";
 
 function UseInputhooks() {
@@ -15,11 +14,16 @@ function UseInputhooks() {
     ];
 
     const useTabs = (initialTab, allTabs) => {
+
+        const [currentIndex, setCurrentIndex] = useState(initialTab); 
+
         if(!allTabs || !Array.isArray(allTabs)){ // 조건이 일치시 리턴
             return; 
         }
         
-        const [currentIndex, setCurrentIndex] = useState(initialTab); 
+        // const [currentIndex, setCurrentIndex] = useState(initialTab); 
+        //여기 있을시 useState오류가 나타 남 오류가 나는데 동작이 잘되면 최상단으로 위치시키면 오류가 안난다.
+
         return{
             currentItem: allTabs[currentIndex], // Item은 Tabs의 값을 리턴하고 index값을 가진다. 
             changeItem: setCurrentIndex // 값 변경 하기 
