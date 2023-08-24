cf9f920cd5695cc2952489f4144d6dca2eb2c1f1
Hongaproject <tkwlscjq99@gmail.com>
2023-08-24 22:49:05 +0900

    axios

--- nomadcoder-React/reacthooks/src/useEffect/Axios.jsx
+++ nomadcoder-React/reacthooks/src/useEffect/Axios.jsx
@@ -3,7 +3,9 @@ import UseAxios from "./UseAxios";
 function Axios() {
 
     const {loading, data, error} = UseAxios({url : "https://yts.mx/api/v2/list_movies.json"});
-    console.log(`Loading: ${loading} \nError: ${error} \nData: ${JSON.stringify(data)}`);
+    console.log(`Loading: ${loading} \nError: ${error} \nData: ${JSON.stringify(data)}`); 
+    // console로 내용을 보여줌 
+    
 
     return(
         <div>
