da387d50d624d9f9a32d2c5914f2f087eeddd178
Hongaproject <tkwlscjq99@gmail.com>
2023-11-16 18:08:48 +0900

    useRef 사용법

--- React-Velopert/src/component/reacthook/HookApp.jsx
+++ React-Velopert/src/component/reacthook/HookApp.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import InputSample from './InputSample';
 
 function HookApp() {
     // reacthook폴더에 있는 컴포넌트 내용을 보여주는 곳 입니다.
   return (
     <div>
-      
+      <InputSample />
     </div>
   );
 }
--- React-Velopert/src/component/reacthook/InputSample.jsx
+++ React-Velopert/src/component/reacthook/InputSample.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 function InputSample() {
   
@@ -20,18 +20,28 @@ function InputSample() {
     title: "",
     content: "",
   });
+
+  const inputName = useRef();
   
   const {title, content} = text1; // 구조분해할당을 사용해 코드를 간략하게 사용함.
 
   const onChange1 = (e) => {
     const {name, value} = e.target // name을 사용하는 이유는 input에 맞는 값을 state에 삽입해야하므로 사용 함.
-    setText({
+    setText1({
       ...text1,
       [name]: value 
       // []로 하는 이유는 문자열 name자체가 들어가는거라 값에 따라 다른 key값을 받아오기 때매 
       // useState값이 여러 개여도 상관 없다.
     });
   };
+
+  const onReset1 = () => {
+    setText1({
+      title: "",
+      content: ""
+    });
+    inputName.current.focus();
+  }
 
   return (
     <div>
@@ -41,8 +51,9 @@ function InputSample() {
         <span>: {text}</span>
       </div>
 
-      <input type="text" name="title" placeholder='테스트 입니다.' onChange={onChange1} value={title} />
+      <input type="text" name="title" placeholder='테스트 입니다.' onChange={onChange1} value={title} autoFocus ref={inputName}/>
       <input type="text" name="content" placeholder='테스트 입니다.' onChange={onChange1} value={content} />
+      <button onClick={onReset1}>reset</button>
 
     </div>
   );
