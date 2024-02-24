5b7f1c210fa2216401f0386e9f75c561a4f21082
Hongaproject <tkwlscjq99@gmail.com>
2024-02-24 16:46:27 +0900

    useReducer, Redux 차이

---
+++ src/rstudy/Redux.jsx
@@ -0,0 +1,19 @@
+function Redux () {
+
+    // useReducer와 Redux와 차이
+    // useReducer는 Context API와 함께 사용한다면 Redux의 기능을 구현 할 수 있다.
+
+    // 우선 useReducer은 내장 함수이고 Redux는 외부 라이브러리이다. 
+    // Redux는 동기적 useReducer은 비동기적
+    
+    // useReducer은 store이 없어 로컬 상태관리로 사용하고
+    // Redux는 store을 통해 관리를 하고 전역상태 관리에 사용합니다.
+
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Redux;
