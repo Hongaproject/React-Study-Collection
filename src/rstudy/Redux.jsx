1164ab483dcf3eb54fe6af42c5b4bed1062f7f6d
Hongaproject <tkwlscjq99@gmail.com>
2024-02-25 14:54:41 +0900

    리덕스 설치 및 내용 설명 추가

--- src/rstudy/Redux.jsx
+++ src/rstudy/Redux.jsx
@@ -9,6 +9,15 @@ function Redux () {
     // useReducer은 store이 없어 로컬 상태관리로 사용하고
     // Redux는 store을 통해 관리를 하고 전역상태 관리에 사용합니다.
 
+    // https://ko.redux.js.org/introduction/why-rtk-is-redux-today/
+
+    // 설치 
+    // npm install @reduxjs/toolkit
+
+    // 사용이유 toolkit
+    // redux의 환경 설정이 불편 - 여러 라이브러리 다운받아 사용해야함  \
+    // 보일러플레이트 코드를 너무 많이 사용함.
+
     return(
         <div>
 
