2a14780cbc7855291d52a205074af18a153dd234
Hongaproject <tkwlscjq99@gmail.com>
2024-04-04 01:17:53 +0900

    리덕스 새로운 모듈 작성

---
+++ React-mysql,redux/MysqlRedux/src/VltRedux/exercise.jsx
@@ -0,0 +1,6 @@
+import { legacy_createStore as createStore } from "redux";
+
+// counter과 todolist에 관한 리듀서 및 스토어 까지 다 넣는 방식을 사용하려고 함. 
+// createStore 밑줄 그어진것은, @reduxjs/toolkit의 권장사용을 유도하기 위해 취소줄이 그어짐.
+// legacy_createStore as createStore 이렇게 사용하면 오류가 나타나지 않음.
+
