dbe37c2b573ff3be1b2b76d9ccb93d7283d05c85
Hongaproject <tkwlscjq99@gmail.com>
2024-05-30 19:21:20 +0900

    map-key 이론 내용

--- React-Velopert/src/App.jsx
+++ React-Velopert/src/App.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Main6 from './router-dom 6/Main6';
 import Main64 from './router-dom 6.4/Main64';
 
+
+
 function App() {
 
+  const contentComments = [
+    {
+      'id'  : 0,
+      'name': 'chanho',
+      'text': '안녕하세요 테스트 중입니다!😝'
+    },
+    {
+      'id'  : 1,
+      'name': 'hoho',
+      'text': '위코드으~~😀😀😀😀'
+    }
+  ]
+
+  
   return (
     <div>
-      {/* <Main6 /> */}
-      <Main64 />
+      {
+        contentComments.map((list) => (
+          <li key={list.id}>
+            {list.name}
+            {list.text}
+          </li>
+        ))
+      }
     </div>
   );
+    
 }
 
 export default App;
+
--- React-Velopert/src/router-dom 6.4/routes/Error64.jsx
+++ React-Velopert/src/router-dom 6.4/routes/Error64.jsx
@@ -16,3 +16,4 @@ export default function Error64 () {
         </div>
     );
 }
+
